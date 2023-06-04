b84f988f6d8ae650f55263afa6b0f48df7afedda
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-06-04 21:48:09 +0200

    init

---
+++ tests/example/example_test.py
@@ -0,0 +1,3 @@
+class TestExample:
+    def test_example(self, example_fixture: str) -> None:
+        assert example_fixture == "example", "I am an example test, please remove me from your test suite"
